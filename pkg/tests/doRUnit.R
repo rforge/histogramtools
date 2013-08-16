b198184960ce1009a6d7eb13eda1f16475e9147c
murray <murray@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-16 20:20:25 +0000

    Style/spacing fix suggested by Tim.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/histogramtools@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/doRUnit.R
+++ pkg/tests/doRUnit.R
@@ -1,6 +1,6 @@
-if(require("RUnit", quietly = TRUE)) {
+if (require("RUnit", quietly = TRUE)) {
   pkg <- "HistogramTools"
-  require( pkg, character.only=TRUE)
+  require(pkg, character.only = TRUE)
   path <- system.file("unitTests", package = pkg)
   stopifnot(file.exists(path), file.info(path.expand(path))$isdir)
   source(file.path(path, "runTests.R"), echo = TRUE)
