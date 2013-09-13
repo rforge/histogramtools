1ee4bfc9aac74f95efa095baece2ac44c8cc7522
murray <murray@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-13 06:50:13 +0000

    Syntax error.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/histogramtools@132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/runtests.R
+++ pkg/tests/runtests.R
@@ -1,10 +1,10 @@
 # Simplest possible RUnit boilerplate for now.
 
-library(c("RUnit", "HistogramTools"))
+library("RUnit")
+library("HistogramTools")
 
 # Make the test deterministic - we use a lot of randomly created histograms.
 set.seed(0)
-path <- system.file("unitTests", package = "HistogramTools")
 
 # Define tests
 testSuite <- defineTestSuite(
@@ -12,11 +12,13 @@ testSuite <- defineTestSuite(
   dirs=system.file("unitTests", package = "HistogramTools"),
   testFuncRegexp = "^[Tt]est.+")
 
+# Run tests
 tests <- runTestSuite(testSuite)
 
 # Print results
 printTextProtocol(tests)
 
-if(getErrors(tests)$nFail > 0) {
-  stop("one of the unit tests failed")
+# Return success or failure to R CMD CHECK
+if (getErrors(tests)$nFail > 0) {
+  stop("TEST FAILED!")
 }
